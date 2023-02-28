0eb2277ba9cd14ee156eb1682fda8f80d616fefa
Fellipe Manaia <100931530+fellipemanaia@users.noreply.github.com>
2023-02-28 12:26:55 -0300

    Realidade Aumentada 28/02

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+function troca1(){
+    window.location.href = "index1.html"
+
+}
+
+function troca2(){
+
+    window.location.href = "index2.html"
+}
+
+function troca3(){
+    window.location.href = "index3.html"
+}
+
+function troca4(){
+    window.location.href = "index4.html"
+}
+
+function troca5(){
+    window.location.href = "index5.html"
+}
+
+
+
+
